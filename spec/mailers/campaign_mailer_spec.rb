c5c08441eecaa461a19d3a730bae33dff43d772f
Tiago Leal <tiago.sleal12@gmail.com>
2019-04-07 09:35:04 -0300

    mailer test

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -1,5 +1,35 @@
+#/spec/mailer/campaign_mailer_spec.rb
 require "rails_helper"
 
 RSpec.describe CampaignMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'raffle' do
+
+    before do
+      @campaign = create(:campaign)
+      @member = create(:member, campaign: @campaign)
+      @email = CampaignMailer.raffle(@campaign,@member,@friend)
+    end
+
+    it 'renders the headers' do
+      expect(@email.subject).to eq("Nosso Amigo Secreto: #{@campaign.title}")
+      expect(@email.to).to eq([@member.email])
+    end
+
+    it 'body have member name' do
+      expect(@email.body.encoded).to match(@match.name)
+    end
+
+    it 'body have campaign creator name' do
+      expect(@email.body.encoded).to match(@campaign.user.name)
+    end
+
+    it 'body have friend name' do
+      expect(@email.body.encoded).to match(@friend.name)
+    end
+
+    it 'body have member link to set open' do
+      expect(@email.body.encoded).to match("/members/#{@member.token}/opened")
+    end
+  end
 end
