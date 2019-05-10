33898e7f34872722e78b179b1ee25ba35eec5636
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-10 00:22:07 -0300

    create the test controller member

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,23 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
-    it "returns http success" do
-      skip
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
+  end
+
+  
+  describe "POST #create" do
+   
+    before(:each) do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      @campaign = create(:campaign, user: @current_user)
+      @new_member_attributes = attributes_for(:member, campaign: @campaign)
+      post :create, params: {member: @new_member_attributes}
     end
+    
+    it { expect{ create(:member,campaign: @campaign) }.to change { Member.count }.by(1) }
+    
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
-      skip
+    before(:each) do
+      request.env["HTTP_ACCEPT"] = 'application/json'
     end
+    
+    context "Delete Member of the campaign" do
+      it "returns http forbidden" do
+        member = create(:member)
+        delete :destroy, params: {id: member.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-     skip
-    end
-  end
+  
+  describe "PUT #update" do
 
+    before(:each) do
+      @new_member_attributes = attributes_for(:member)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    context "edit attributes of the member" do
+      before(:each) do 
+        campaign = create(:campaign, user: @current_user)
+        member = create(:member, campaign:campaign)
+        put :update, params: {id: member.id, member: @new_member_attributes}
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "Member have the new attributes" do
+        expect(Member.last.name).to  eq(@new_member_attributes[:name])
+        expect(Member.last.email).to eq(@new_member_attributes[:email])
+      end
+    
+    end  
+
+
+  end
+  
 end
